bfb85882bc3ca83d31d40703c57d4ced82503d99
Jason Goemaat <jason.goemaat@gmail.com>
2021-10-01 19:24:25 -0500

    Basic firestore rules (I can do anything), firestore working in chrome console

--- simple/public/app.js
+++ simple/public/app.js
@@ -1,8 +1,9 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-auth.js";
+import { getFirestore, addDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
 
 console.log('loading module app.js');
 
-let myApp = function(app, getAuth) {
+let myApp = function(app) {
   console.log('app.js - init()');
   console.log('app (returned by initializeApp):', app);
   console.log('getAuth:', getAuth);
@@ -24,7 +25,28 @@ let myApp = function(app, getAuth) {
     .then(result => console.log('auth result:', result))
     .catch(error => console.log('auth error:', error)));
 
-  signOutBtn.addEventListener('click', () => auth.SignOut());
+  signOutBtn.addEventListener('click', () => auth.signOut());
+
+  auth.onAuthStateChanged(user => {
+    window['user'] = user;
+    if (user) {
+      whenSignedIn.hidden = false;
+      whenSignedOut.hidden = true;
+      userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3><p>User ID: ${user.uid}</p>`;
+    } else {
+      whenSignedIn.hidden = true;
+      whenSignedOut.hidden = false;
+      userDetails.innerHTML = '';
+    }
+  });
+
+  const db = getFirestore();
+  window['db'] = db;
+  window['fs'] = { addDoc, collection, getDocs };
+  for (let key in window['fs']) {
+    window[key] = window['fs'][key];
+  }
+  console.log('db:', db);
 }
 
 export { myApp };
